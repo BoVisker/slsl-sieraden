7bccab58b1aa040e16713dfdcd280668fc09692a
Bo Visker <visk3rtjes@gmail.com>
2024-03-26 18:32:34 +0100

    toegevoegt: knop in sieraad-details om terug naar de homepagina te gaan

--- src/app/sieraden.ts
+++ src/app/sieraden.ts
@@ -1,11 +1,10 @@
 import { Sieraad } from './sieraad';
 
 export const Sieraden: Sieraad[] = [
-  { id: 1, name: 'Colourfull katsuki Kralen', price: '2.00', imgURL: "../../assets/producten/ColourfullKatsukiKralen.jpg", beschrijving: "test beschrijving" },
-  { id: 2, name: 'CowboyBoods', price: '3.50', imgURL: "../../assets/producten/CowbodyBoods.jpg", beschrijving: "test beschrijving" },
-  { id: 3, name: 'Gifgroen Smiley Armbadje', price: '2.00', imgURL: "../../assets/producten/GifgroenSmileyArmbandje.jpg", beschrijving: "test beschrijving" },
-  { id: 4, name: 'Goudenhartjes Oorbellen', price: '3.50', imgURL: "../../assets/producten/GoudenhartjesOorbellen.jpg", beschrijving: "test beschrijving" },
-  { id: 5, name: 'Parel Armbandje', price: '2.50', imgURL: "../../assets/producten/ParelArmbandje.jpg", beschrijving: "test beschrijving" },
-  { id: 6, name: 'Simpel Carnaval', price: '2.00', imgURL: "../../assets/producten/SimpelCarnaval.jpg", beschrijving: "test beschrijving" },
-  { id: 7, name: 'Sluiting Stickers', price: '00.50', imgURL: "../../assets/producten/SluitingStickers.jpg", beschrijving: "test beschrijving" },
+  { id: 1, name: 'Colourfull katsuki Kralen', price: '2.00', imgURL: "../../assets/producten/ColourfullKatsukiKralen.jpg", beschrijving: "" },
+  { id: 2, name: 'CowboyBoods', price: '3.50', imgURL: "../../assets/producten/CowbodyBoods.jpg", beschrijving: "" },
+  { id: 3, name: 'Gifgroen Smiley Armbadje', price: '2.00', imgURL: "../../assets/producten/GifgroenSmileyArmbandje.jpg", beschrijving: "" },
+  { id: 4, name: 'Goudenhartjes Oorbellen', price: '3.50', imgURL: "../../assets/producten/GoudenhartjesOorbellen.jpg", beschrijving: "" },
+  { id: 5, name: 'Parel Armbandje', price: '2.50', imgURL: "../../assets/producten/ParelArmbandje.jpg", beschrijving: "" },
+  { id: 6, name: 'Simpel Carnaval', price: '2.00', imgURL: "../../assets/producten/SimpelCarnaval.jpg", beschrijving: "" },
 ];                    
