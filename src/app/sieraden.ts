f5ade73916d4fbd93d125514a7c03af2320b60ce
Daan Visker <d.visker@kader.nl>
2024-03-27 10:54:39 +0100

    Schrijf fouten, naamgeving sierraden & sierraad classes

--- src/app/sieraden.ts
+++ src/app/sieraden.ts
@@ -1,11 +1,11 @@
 import { Sieraad } from './sieraad';
 
 export const Sieraden: Sieraad[] = [
-  { id: 1, name: 'Colourfull katsuki Kralen', price: '2.00', imgURL: "../../assets/producten/ColourfullKatsukiKralen.jpg", beschrijving: "test beschrijving" },
-  { id: 2, name: 'CowboyBoods', price: '3.50', imgURL: "../../assets/producten/CowbodyBoods.jpg", beschrijving: "test beschrijving" },
-  { id: 3, name: 'Gifgroen Smiley Armbadje', price: '2.00', imgURL: "../../assets/producten/GifgroenSmileyArmbandje.jpg", beschrijving: "test beschrijving" },
-  { id: 4, name: 'Goudenhartjes Oorbellen', price: '3.50', imgURL: "../../assets/producten/GoudenhartjesOorbellen.jpg", beschrijving: "test beschrijving" },
-  { id: 5, name: 'Parel Armbandje', price: '2.50', imgURL: "../../assets/producten/ParelArmbandje.jpg", beschrijving: "test beschrijving" },
-  { id: 6, name: 'Simpel Carnaval', price: '2.00', imgURL: "../../assets/producten/SimpelCarnaval.jpg", beschrijving: "test beschrijving" },
-  { id: 7, name: 'Sluiting Stickers', price: '00.50', imgURL: "../../assets/producten/SluitingStickers.jpg", beschrijving: "test beschrijving" },
-];                    
+  { id: 1, name: 'Colourful Katsuki Kralen', price: '2.00', imgURL: "../../assets/producten/ColourfullKatsukiKralen.jpg", beschrijving: "test beschrijving" },  // TODO: beschrijving toevoegen
+  { id: 2, name: 'Cowboy Boots', price: '3.50', imgURL: "../../assets/producten/CowbodyBoods.JPG", beschrijving: "test beschrijving" },                         // TODO: beschrijving toevoegen
+  { id: 3, name: 'Gifgroen Smiley Armbandje', price: '2.00', imgURL: "../../assets/producten/GifgroenSmileyArmbandje.jpg", beschrijving: "test beschrijving" }, // TODO: beschrijving toevoegen
+  { id: 4, name: 'Gouden Hartjes Oorbellen', price: '3.50', imgURL: "../../assets/producten/GoudenhartjesOorbellen.jpg", beschrijving: "test beschrijving" },   // TODO: beschrijving toevoegen
+  { id: 5, name: 'Parel Smiley Armbandje', price: '2.50', imgURL: "../../assets/producten/ParelArmbandje.jpg", beschrijving: "test beschrijving" },             // TODO: beschrijving toevoegen
+  { id: 6, name: 'Oeteldonks Carnaval Armbandje', price: '2.00', imgURL: "../../assets/producten/SimpelCarnaval.jpg", beschrijving: "test beschrijving" },      // TODO: beschrijving toevoegen
+  { id: 7, name: 'Sluiting Stickers', price: '00.50', imgURL: "../../assets/producten/SluitingStickers.jpg", beschrijving: "test beschrijving" },               // TODO: beschrijving toevoegen
+];
